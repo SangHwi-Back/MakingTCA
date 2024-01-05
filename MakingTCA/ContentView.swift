3feb8381b44d76c808f8472636683b9c48e3c489
백상휘 <sanghwiback@gmail.com>
2024-01-05 10:34:54 +0900

    Project insert

---
+++ MakingTCA/ContentView.swift
@@ -0,0 +1,19 @@
+//
+//  ContentView.swift
+//  MakingTCA
+//
+//  Created by 백상휘 on 2024/01/05.
+//
+
+import SwiftUI
+import CoreData
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello world!")
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MakingTCA/MakingTCAApp.swift
@@ -0,0 +1,18 @@
+//
+//  MakingTCAApp.swift
+//  MakingTCA
+//
+//  Created by 백상휘 on 2024/01/05.
+//
+
+import SwiftUI
+
+@main
+struct MakingTCAApp: App {
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
